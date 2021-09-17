57df12521784e3b5d3c36cd9777860bff3b294b3
Huvinesh-Rajendran <huvitom99@outlook.com>
2021-09-17 09:45:58 +0800

    practical2 modified

--- prac2.R
+++ prac2.R
@@ -5,3 +5,13 @@ Marks2 <- c(90,90)
 students <- data.frame(Name,Age,Marks1,Marks2)
 students 
 str(students)
+# convert the data frame to matrix
+# vector > 1 dimension 
+# matrix > 2 dim 
+# list > combination
+mat1 <- data.matrix(students)
+mat1
+
+x <- data.frame(foo=1:4,bar=c(T,T,F,F))
+x 
+ncol(x)
